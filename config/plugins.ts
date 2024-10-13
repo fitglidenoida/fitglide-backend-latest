802c699f9e2c109f2c35417ec07cf4abe846bd95
prince <prince.dwivedi91@gmail.com>
2024-10-13 11:33:31 +0530

    fixed register api

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
 export default () => ({});
+module.exports = ({ env }) => ({
+    // ...
+    "users-permissions": {
+      config: {
+        register: {
+          allowedFields: ["mobile"],
+        },
+      },
+    },
+    // ...
+  });
