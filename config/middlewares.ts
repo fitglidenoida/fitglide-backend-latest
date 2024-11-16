e0527f00dff5edf1e1a886aece887239f047a629
fitglidenoida <164656997+fitglidenoida@users.noreply.github.com>
2024-11-16 21:07:10 +0530

    middleware update

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,19 +1,23 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:3000','https://fitglide.in'], // Frontend URL
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      credentials: true,
-    },
+'strapi::logger',
+'strapi::errors',
+'strapi::security',
+{
+  name: 'strapi::cors',
+  config: {
+    origin: [
+      'http://localhost:3000',  // Local development
+      'https://fitglide.in',    // Production frontend
+      'https://admin.fitglide.in' // Backend
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
   },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+},
+'strapi::poweredBy',
+'strapi::query',
+'strapi::body',
+'strapi::session',
+'strapi::favicon',
+'strapi::public',
 ];
