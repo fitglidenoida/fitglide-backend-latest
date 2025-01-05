1ed61840287b472ee193ed83c68a98d72643aa33
fitglidenoida <info@fitglide.in>
2025-01-05 23:33:26 +0530

    updated plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     "users-permissions": {
       config: {
         register: {
-          allowedFields: ["username", "email", "password", "First_name", "Last_name", "mobile"],
+          allowedFields: ["mobile"],
           
         },
       },
